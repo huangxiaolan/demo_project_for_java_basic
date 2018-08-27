9ae116557c712ad6117953ff47cc7548fa4c7ca4
huangxiaolan <1257830250@qq.com>
2018-08-27 22:10:14 +0800

    add netty socket io protobuf demo

--- netty-socket-io-protobuf-demo/README.md
+++ netty-socket-io-protobuf-demo/README.md
@@ -7,3 +7,80 @@
 前端使用https://github.com/dcodeIO/ProtoBuf.js 项目作为解析protobuf的工具；
 
 使用https://github.com/mrniko/netty-socketio提供的工具访问socket.io协议。
+
+#socket_server java实现的socketio服务端
+
+需要先编译protobuf协议，然后启动App.java
+
+#socket_client js实现的socketio客户端
+
+直接打开index.html页面，chrome打开本地index.html打开时需要配置参数 --allow-file-access-from-files ,以支持本地跨域。
+
+#socketio_connect_and_close.jpg 展示socket协议建立连接到关闭连接的过程
+
+# socket io协议过程
+
+先阅读websocket协议， 然后参考engineio协议https://github.com/socketio/engine.io-protocol，参考socketio协议https://github.com/socketio/socket.io-protocol
+
+下面简单描述socketio协议过程：
+
+![avatar](socketio_connect_and_close.jpg)
+
+socketio 支持三者模式， websocket ，polling，ajax 。
+
+以下是socketio（websocket模式）整个通讯的过程：
+
+1  客户端发起socketio连接
+
+2  tcp三次握手建立连接
+
+3  websocket协议建立连接（特定的http协议，一次http请求，一次http响应，这里不展开）
+
+4  engine.io建立连接，服务器返回一个websocket帧,附带的值为（0{"sid":"50b56be3-dca8-4fe6-a43c-999e69d9c4c0","upgrades":["websocket"],"pingInterval":25000,"pingTimeout":60000}）
+
+5  socketio建立连接，服务器返回websocket帧，附带值40
+
+6  客户端可以传输非二进制事件并且要求立即响应，格式为
+
+   420["string_send","string send message"]
+
+   4是engineio的消息，2表示socketio的非二进制事件，0表示消息的id，递增，响应时要一一对应上。["string_send","string send message"]表示数组，第一个值是事件id，后面是事件的参数
+
+   服务器可以响应非二进制事件，格式为
+
+   430["string_send_ack data"]
+
+   4是engineio的消息，3表示socketio的非二进制响应，0表示消息的id，递增，对应发送的消息。["string_send_ack data"]表示数组，响应的数据
+
+   传输非二进制不要求立即响应事件，格式为
+
+   42["string_send","string send message"]
+
+   4是engineio的消息，2表示socketio的非二进制事件。["string_send","string send message"]表示数组，第一个值是事件id，后面是事件的参数
+
+   二进制事件有两个websocket帧：
+
+   二进制非立即响应事件，格式为
+   第一帧：
+
+   451-["byte_send_without_ack",{"_placeholder":true,"num":0}]
+
+   4是engineio的消息，51-表示socketio的二进制事件。["byte_send_without_ack",{"_placeholder":true,"num":0}]数组第一个参数是事件id，后面是固定值
+
+   第二帧：
+
+   二进制数据
+
+   二进制立即响应事件，格式为
+   第一帧为：
+   451-2["byte_send_without_ack",{"_placeholder":true,"num":0}]
+
+   比上面多了2表示事件的id
+
+7  socketio断开连接（websocket帧，41）
+
+8  websocket帧，值为2表示websocket的ping，值为3表示websocket的pong
+
+9  engine断开连接，websocket帧1
+
+10 tcp断开连接
--- netty-socket-io-protobuf-demo/socket.server/src/main/java/com/hxiaol/socket/server/App.java
+++ netty-socket-io-protobuf-demo/socket.server/src/main/java/com/hxiaol/socket/server/App.java
@@ -24,23 +24,77 @@ public class App
         //构造服务器
         final SocketIOServer server = new SocketIOServer(config);
         //添加监听事件
-        server.addEventListener("user_req", byte[].class, new DataListener<byte []>() {
+        server.addEventListener("byte_send", byte[].class, new DataListener<byte []>() {
             
             public void onData(SocketIOClient client, byte [] bs, AckRequest ackRequest) {
-                
                 try {
-                    
+
+                    Response response=Response.newBuilder().setCode(0).setMsg("byte_send byte[]").build();
+                    if(ackRequest.isAckRequested()){
+                        ackRequest.sendAckData(response.toByteArray());
+
+                    }
+                    //解析字节为对象
+                    User user=User.parseFrom(bs);
+                    System.out.println("receive user : "+user);
+
+                } catch (InvalidProtocolBufferException e) {
+                    throw new RuntimeException("byte_send 数据处理异常",e);
+                }
+                
+                
+            }
+        });
+
+        //添加监听事件
+        server.addEventListener("byte_send_without_ack", byte[].class, new DataListener<byte []>() {
+
+            public void onData(SocketIOClient client, byte [] bs, AckRequest ackRequest) {
+                try {
+
+                    Response response=Response.newBuilder().setCode(0).setMsg("byte_send byte[]").build();
+
                     //解析字节为对象
                     User user=User.parseFrom(bs);
                     System.out.println("receive user : "+user);
                     //构造返回结果并且发送回前端
-                    Response response=Response.newBuilder().setCode(0).setMsg("user_res event").build();
-                    client.sendEvent("user_res",response.toByteArray());
+
+                    client.sendEvent("byte_send_without_ack_res",response.toByteArray());
                 } catch (InvalidProtocolBufferException e) {
-                    throw new RuntimeException("user_req 数据处理异常",e);
+                    throw new RuntimeException("byte_send_without_ack 数据处理异常",e);
                 }
-                
-                
+
+
+            }
+        });
+
+        //添加监听事件
+        server.addEventListener("string_send", String.class, new DataListener<String>() {
+
+            public void onData(SocketIOClient client, String data, AckRequest ackRequest) {
+
+                if(ackRequest.isAckRequested()){
+                    ackRequest.sendAckData("string_send_ack data");
+                }
+                //解析字节为对象
+
+                System.out.println("receive data : "+data);
+                //构造返回结果并且发送回前端
+
+            }
+        });
+
+        //添加监听事件
+        server.addEventListener("string_send_without_ack", String.class, new DataListener<String>() {
+
+            public void onData(SocketIOClient client, String data, AckRequest ackRequest) {
+
+                //解析字节为对象
+
+                System.out.println("receive data : "+data);
+                //构造返回结果并且发送回前端
+
+                client.sendEvent("string_send_without_ack_res",data);
             }
         });
         
